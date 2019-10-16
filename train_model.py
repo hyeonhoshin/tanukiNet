b4c60dc8b1f03123ad5398873a3ebaec17e174fe
Tanukimong <tlsgusghq@me.com>
2019-10-16 13:44:30 +0900

    append -> concatenate

--- train_model.py
+++ train_model.py
@@ -24,7 +24,7 @@ input_shape = train_images.shape[1:]
 # Make into arrays as the neural network wants these
 labels = labels[..., np.newaxis]
 blanks = np.zeros_like(labels)
-labels = np.append(labels,blanks, axis = 3)
+labels = np.concatenate(labels, blanks, axis = 3)
 
 print("Train_imgs is {}, labels is {}".format(train_images.shape, labels.shape))
 
