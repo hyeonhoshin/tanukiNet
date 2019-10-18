f5680a7d41923087fa104da0487c572be8f9c35f
Tanukimong <tlsgusghq@me.com>
2019-10-18 15:38:55 +0900

    다시 unet 실험 모델 구성 시작

--- tanuki_ml.py
+++ tanuki_ml.py
@@ -40,73 +40,50 @@ def give_time(X, y, memory_size = 3):
     return X_t[:end_idx], y_t[:end_idx]
 
 def generate_model(input_shape, pool_size):
-    ### Here is the actual neural network ###
-    model = Sequential()
-    # Normalizes incoming inputs. First layer needs the input shape to work
-    model.add(BatchNormalization(input_shape=input_shape))
 
-    # Below layers were re-named for easier reading of model summary; this not necessary
-    # Conv Layer 1
-    model.add(TimeDistributed(Conv2D(60, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Conv1')))
-    # Conv Layer 2
-    model.add(TimeDistributed(Conv2D(50, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Conv2')))
+    inputs = Input(input_shape)
+    conv1 = Conv2D(8, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(8, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=pool_size)(conv1)
 
-    # Pooling 1
-    model.add(TimeDistributed(MaxPooling2D(pool_size=pool_size)))
+    conv2 = Conv2D(16, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(pool1)
+    drop1 = Dropout(0.2)(conv2)
+    conv2 = Conv2D(16, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop1)
+    drop2 = Dropout(0.2)(conv2)
+    conv2 = Conv2D(16, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop2)
+    drop3 = Dropout(0.2)(conv2)
+    pool2 = MaxPooling2D(pool_size=pool_size)(drop3)
 
-    # Conv Layer 3
-    model.add(TimeDistributed(Conv2D(40, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Conv3')))
-    # Conv Layer 4
-    model.add(TimeDistributed(Conv2D(30, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Conv4')))
-    # Conv Layer 5
-    model.add(TimeDistributed(Conv2D(20, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Conv5')))
-    # Pooling 2
-    model.add(TimeDistributed(MaxPooling2D(pool_size=pool_size)))
+    conv3 = Conv2D(32, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(pool2)
+    drop4 = Dropout(0.2)(conv3)
+    conv3 = Conv2D(32, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop4)
+    drop5 = Dropout(0.2)(conv3)
+    pool3 = MaxPooling2D(pool_size=pool_size)(drop5)
 
-    # Conv Layer 6
-    model.add(ConvLSTM2D(filters=20, kernel_size=(3, 3), strides=(1, 1), activation='relu', data_format='channels_last',
-                         padding='same', return_sequences=True))
+    up = UpSampling2D(size = pool_size)(pool3)
 
-    model.add(ConvLSTM2D(filters=10, kernel_size=(3, 3), strides=(1, 1), activation='relu', data_format='channels_last',
-                         padding='valid', return_sequences=True))
+    up6 = Conv2D(64, 2, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')()
+    deconv1 = Conv2DTranspose(64, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Deconv1')(up6)
+    drop5 = Dropout(0.2)(deconv1)
+    deconv1 = Conv2DTranspose(64, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Deconv1')(drop5)
+    drop5 = Dropout(0.2)(deconv1)
 
-    # Conv Layer 7
-    model.add(ConvLSTM2D(filters=5, kernel_size=(3, 3), strides=(1, 1), activation='relu', data_format='channels_last',
-                         padding='valid', return_sequences=False))
+    up = UpSampling2D(size = pool_size)(drop5)
+    deconv1 = Conv2DTranspose(32, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Deconv1')(up)
+    drop5 = Dropout(0.2)(deconv1)
+    deconv1 = Conv2DTranspose(32, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Deconv1')(deconv1)
+    drop5 = Dropout(0.2)(deconv1)
+    deconv1 = Conv2DTranspose(16, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Deconv1')(drop5)
+    drop5 = Dropout(0.2)(deconv1)
 
-    # Pooling 3
-    model.add(MaxPooling2D(pool_size=pool_size))
+    up = UpSampling2D(size = pool_size)(drop5)
+    deconv1 = Conv2DTranspose(16, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Deconv1')(up)
+    drop5 = Dropout(0.2)(deconv1)
+    deconv1 = Conv2DTranspose(1, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Deconv1')(deconv1)
+    drop5 = Dropout(0.2)(deconv1)
 
-    # Upsample 1
-    model.add(UpSampling2D(size=pool_size))
-
-    # Deconv 1
-    model.add(Conv2DTranspose(10, (3, 3), padding='valid', strides=(1, 1), activation='relu',output_shape = model.layers[8].output_shape[1:], name='Deconv1'))
-
-    # Deconv 2
-    model.add(Conv2DTranspose(20, (3, 3), padding='valid', strides=(1, 1), activation='relu',output_shape = model.layers[7].output_shape[1:], name='Deconv2'))
-
-    # Upsample 2
-    model.add(UpSampling2D(size=pool_size))
-
-    # Deconv 3
-    model.add(Conv2DTranspose(30, (3, 3), padding='valid', strides=(1, 1), activation='relu',output_shape = model.layers[5].output_shape, name='Deconv3'))
-
-    # Deconv 4
-    model.add(Conv2DTranspose(40, (3, 3), padding='valid', strides=(1, 1), activation='relu',output_shape = model.layers[4].output_shape, name='Deconv4'))
-
-    # Deconv 5
-    model.add(Conv2DTranspose(50, (3, 3), padding='valid', strides=(1, 1), activation='relu',output_shape = model.layers[3].output_shape, name='Deconv5'))
-
-    # Upsample 3
-    model.add(UpSampling2D(size=pool_size))
-
-    # Deconv 6
-    model.add(Conv2DTranspose(60, (3, 3), padding='valid', strides=(1, 1), activation='relu',output_shape = model.layers[1].output_shape, name='Deconv6'))
-
-    # Final layer - only including one channel so 1 filter
-    model.add(Conv2DTranspose(1, (3, 3), padding='valid', strides=(1, 1), activation='relu',output_shape = model.layers[0].output_shape, name='Final'))
-
+    model.compile(optimizer='Adam', loss='mean_squared_error')
+    
     return model
 
 def read_set(target, resized_shape):
@@ -306,58 +283,5 @@ def generate_init_LSTM_model(input_shape, pool_size):
 
     # Final layer - only including one channel so 1 filter
     model.add(Conv2DTranspose(1, (3, 3), padding='valid', strides=(1, 1), activation='relu', name='Final'))
-
-    return model
-
-def unet(pretrained_weights = None,input_size = (256,256,1)):
-    inputs = Input(input_size)
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
-
-    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-    merge6 = concatenate([drop4,up6], axis = 3)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-
-    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-    merge7 = concatenate([conv3,up7], axis = 3)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-
-    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-    merge8 = concatenate([conv2,up8], axis = 3)
-    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-
-    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-    merge9 = concatenate([conv1,up9], axis = 3)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
-
-    model = Model(input = inputs, output = conv10)
-
-    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
-    
-    #model.summary()
-
-    if(pretrained_weights):
-    	model.load_weights(pretrained_weights)
 
     return model
