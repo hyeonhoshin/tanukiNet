ec1897c208f1c00158ce5674d508171cf195d982
Tanukimong <tlsgusghq@me.com>
2019-10-29 01:42:44 +0900

    원본과 끝단도 동일하도록 세팅

--- tanuki_ml.py
+++ tanuki_ml.py
@@ -153,10 +153,10 @@ def generate_model(input_shape, pool_size, weight_decay=0.):
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
     # Convolutional layers transfered from fully-connected layers
-    x = Conv2D(1024,(7, 7), activation='relu', padding='same', dilation_rate=(2, 2),
+    x = Conv2D(2048,(7, 7), activation='relu', padding='same', dilation_rate=(2, 2),
                       name='fc1', kernel_regularizer=l2(weight_decay))(x)
     x = Dropout(0.5)(x)
-    x = Conv2D(1024, (1, 1), activation='relu', padding='same', dilation_rate=(2, 2), name='fc2', kernel_regularizer=l2(weight_decay))(x)
+    x = Conv2D(2048, (1, 1), activation='relu', padding='same', dilation_rate=(2, 2), name='fc2', kernel_regularizer=l2(weight_decay))(x)
     x = Dropout(0.5)(x)
     #classifying layer
     x = Conv2D(1, (1, 1), kernel_initializer='he_normal', activation='linear', padding='valid', strides=(1, 1), kernel_regularizer=l2(weight_decay))(x)
