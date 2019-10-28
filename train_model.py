118a749d7dc061263dae85874e21b253c8508cc4
Tanukimong <tlsgusghq@me.com>
2019-10-28 16:48:23 +0900

    History 저장 기능 추가

--- train_model.py
+++ train_model.py
@@ -41,7 +41,7 @@ model.summary()
 
 # 학습
 start_train = time.time()
-model.fit(X_train, y_train, batch_size, epochs, shuffle = True)
+hist = model.fit(X_train, y_train, batch_size, epochs, shuffle = True)
 end_train = time.time()
 
 end_total = time.time()
@@ -67,3 +67,7 @@ min, sec = divmod(end_train-start_train, 60)
 f.write("Pure training time : {}min {}sec\n".format(int(min),int(sec)))
 
 f.close()
+
+# History 저장
+with open('history.p','wb') as f :
+    pickle.dump(hist, f, protocol=4)
