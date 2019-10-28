4d982d3c437459397900083d31333367dc752758
Tanukimong <tlsgusghq@me.com>
2019-10-29 01:12:10 +0900

    Large set에서 원래 셋으로 training

--- train_model.py
+++ train_model.py
@@ -1,53 +1,75 @@
+'''
+모델을 생성하고 학습시킨다.
+폴더 하나씩에 대해서 학습시킨다.
+
+python train_model_with_mem 5
+'''
+
 # File open
 ## Memory 절약을 위해 중간 평가는 Test set이 아니라, Validation split으로 진행
+import os
+from PIL import Image
 import numpy as np
-import pickle
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
-
-# Import necessary items from Keras
-from keras.models import Sequential
-from keras.layers import Activation, Dropout, UpSampling2D
-from keras.layers import Conv2DTranspose, Conv2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
-from keras.preprocessing.image import ImageDataGenerator
-
-from loss_functions import softmax_sparse_crossentropy_ignoring_last_label
-
 import tanuki_ml
+import sys
+import time
+import pickle
+
+from sklearn.utils import shuffle
+start_total = time.time()
+
+# 초기화
+color_num = 3
+scaler = 6
+input_shape = (590//scaler, 1640//scaler, color_num)
+resized_shape = (1640//scaler, 590//scaler)
+batch_size = 32
+epochs = 10
+pool_size = (2, 2)
 
 print("Training start")
 
 # Load training images
-X_train,labels, _ = pickle.load(open("large_train.p", "rb" ))
-
-input_shape = X_train.shape[1:]
-
-# Make into arrays as the neural network wants these
-labels = labels[..., np.newaxis]
-
-print("Train_imgs is {}, labels is {}".format(X_train.shape, labels.shape))
-
-# Normalize labels - training images get normalized to start in the network
-labels = labels / 255
-
-# Batch size, epochs and pool size below are all paramaters to fiddle with for optimization
-batch_size = 32
-epochs = 3
-pool_size = (2, 2)
-input_shape = X_train.shape[1:]
+X_train, y_train, _ = pickle.load(open("tanuki_train.p", "rb" ))
+y_train = y_train[:, 1:-1,:-1, np.newaxis]/255.0
 
 # Model generation
 model = tanuki_ml.generate_model(input_shape, pool_size)
-model.compile(optimizer='Adam', loss='mean_squared_error', metrics = ['accuracy'])
 model.summary()
 
-# 학습
-model.fit(X_train,labels, batch_size, epochs, shuffle = True, validation_split = 0.1)
+# Load test data
+X_test, y_test, _ = pickle.load(open("tanuki_test.p", "rb" ))
+y_test = y_test[:, 1:-1,:-1, np.newaxis]/255.0
 
-# Save model architecture and weights
+# 학습
+start_train = time.time()
+hist = model.fit(X_train, y_train, batch_size, epochs, validation_data = (X_test, y_test), shuffle = True)
+end_train = time.time()
+
+end_total = time.time()
+
+# Weights 저장
+model.save_weights("tanukiNetv1.h5")
+print("Saved model to disk")
+
+# Model 구조 저장
 model_json = model.to_json()
-with open("model.json", "w") as json_file:
+with open("tanukiNetv1.json", "w") as json_file :
     json_file.write(model_json)
 
-model.save_weights('model.h5')
+## 실험 데이터 저장
+f=open("train_result.txt",'w')
+
+# 총 걸린 시간
+min, sec = divmod(end_total-start_total, 60)
+f.write("Total run time : {}min {}sec\n".format(int(min),int(sec)))
+
+# 총 걸린 학습 시간
+min, sec = divmod(end_train-start_train, 60)
+f.write("Pure training time : {}min {}sec\n".format(int(min),int(sec)))
+
+f.close()
+
+# History 저장
+with open('history.p','wb') as f :
+    pickle.dump(hist.history, f, protocol=4)
