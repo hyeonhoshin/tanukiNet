984ef57b4664ef22fe5298ef839e5baacf1fb732
Tanukimong <tlsgusghq@me.com>
2019-11-01 17:47:29 +0900

    오타 수정

--- draw_lanes.py
+++ draw_lanes.py
@@ -55,7 +55,7 @@ def road_lines(image):
         lanes.recent_fit = lanes.recent_fit[1:]
 
     # Calculate average detection
-    lanes.avg_fit = np.average(np.array([i for i in lanes.recent_fit]), axis = 0, weights=self.weights)
+    lanes.avg_fit = np.average(np.array([i for i in lanes.recent_fit]), axis = 0, weights=lanes.weights)
 
     # Generate fake R & B color dimensions, stack with G
     blanks = np.zeros_like(lanes.avg_fit)
